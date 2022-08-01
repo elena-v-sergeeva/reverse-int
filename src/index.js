cc892785d861491b24bf7816fe1ab2deebafba08
Elena Sergeeva <mediagold@mail.ru>
2022-08-01 15:19:18 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+    let result = 0;
+    for(; n; n = Math.floor(n / 10)) {
+        result *= 10;
+        result += n % 10;
+    }
   
+    return result;
 }
